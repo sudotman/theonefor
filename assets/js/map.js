7df01760141f36cab6d627d2eb60de967d898c6f
Satyam Kashyap <satyamsudo@gmail.com>
2025-08-31 21:54:12 +0530

    update GeoGuessr link in map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,6 @@
 // Set your GeoGuessr link here
 // Example: https://www.geoguessr.com/maps/your-custom-map-id
-const GEO_GUESSSR_URL = 'https://www.geoguessr.com/';
+const GEO_GUESSSR_URL = 'https://www.geoguessr.com/maps/68b43361232c9c82955e24c5';
 
 const openBtn = document.getElementById('openMap');
 openBtn.href = GEO_GUESSSR_URL;
